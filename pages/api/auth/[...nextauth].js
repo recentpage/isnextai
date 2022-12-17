2a5a793eb07f414f3ee03940538226d8d16b67ca
recentpage <108588492+recentpage@users.noreply.github.com>
2022-12-17 11:05:13 +0545

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,10 +12,16 @@ export default NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            httpOptions: {
+                timeout: 40000,
+            },
         }),
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+            httpOptions: {
+                    timeout: 40000,
+            },
         }),
     ],
 })
